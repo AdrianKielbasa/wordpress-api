83d0f0755021ab41dbeadbdaff60bc416cf371de
Adrian Kiełbasa <adrian.post.office@gmail.com>
2022-05-11 20:19:54 +0200

    Adding JWT Auth plugin configuration and force https

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -82,7 +82,7 @@ $table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
-
+define('JWT_AUTH_SECRET_KEY', 'your-top-secret-key');
 
 
 /* That's all, stop editing! Happy publishing. */
