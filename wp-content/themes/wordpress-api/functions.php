63232f01f353db1f4931d09e2bb185b524dd61e0
Adrian Kiełbasa <adrian.post.office@gmail.com>
2022-05-15 14:59:41 +0200

    Excluding the token validation url from Bearer Authorization

--- wp-content/themes/wordpress-api/functions.php
+++ wp-content/themes/wordpress-api/functions.php
@@ -8,9 +8,14 @@ add_filter('rest_authentication_errors', function($result) {
         return $result;
     }
 
+    $requestUri = $_SERVER['REQUEST_URI'];
+
     // No authentication has been performed yet.
     // Return an error if user is not logged in.
-    if (!is_user_logged_in()) {
+    if (
+        !is_user_logged_in() &&
+        stripos($requestUri, '/jwt-auth/v1/token/validate') === false
+    ) {
         return new WP_Error(
             'rest_not_logged_in',
             __('You are not currently logged in.'),
@@ -27,13 +32,13 @@ add_filter('rest_authentication_errors', function($result) {
  * Change the token's expire value.
  *
  * @param int $expire The default "exp" value in timestamp.
- * @param int $issued_at The "iat" value in timestamp.
+ * @param int $issuedAt The "iat" value in timestamp.
  *
  * @return int The "nbf" value.
  */
 add_filter(
     'jwt_auth_expire',
-    function ($expire, $issued_at) {
+    function ($expire, $issuedAt) {
         // Modify the "expire" here.
         return time() + (MINUTE_IN_SECONDS * 10);
     },
